5fdd32b6f882bd6d302cc8d3d2fbfed5575c7769
Anmol Singh <bxjjjzid@gmail.com>
2024-10-28 16:10:15 +0530

    Update vars.py

--- WebStreamer/vars.py
+++ WebStreamer/vars.py
@@ -22,7 +22,7 @@ class Var(object):
     HAS_SSL = str(environ.get("HAS_SSL", "0").lower()) in ("1", "true", "t", "yes", "y")
     NO_PORT = str(environ.get("NO_PORT", "0").lower()) in ("1", "true", "t", "yes", "y")
     FQDN = str(environ.get("FQDN", BIND_ADDRESS))
-    URL = "http{}://{}{}/".format(
+    URL = "https://newf2l-3087871c4c8b.herokuapp.com/".format(
             "s" if HAS_SSL else "", FQDN, "" if NO_PORT else ":" + str(PORT)
         )
 
